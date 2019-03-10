afd31492996720e129bd625d716541cbb01d5f9e
Shuoxuan Dong <30633351+Shuoxuan@users.noreply.github.com>
2019-03-10 17:31:02 -0500

    Update stage1_pipeline.sh

--- Stage1/stage1_pipeline.sh
+++ Stage1/stage1_pipeline.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-# written by Yaqi Zhang (zhang623@wisc.edu)
-# University of Wisconsin-Madison
 
 echo "Creating the feature vectors... "
 python ./src/generate_sample.py ./data/train_data ./files/train_positive.dat pos
 python ./src/generate_sample.py ./data/train_data ./files/train_negative.dat neg
 python ./src/generate_sample.py ./data/test_data ./files/test_positive.dat pos
 python ./src/generate_sample.py ./data/test_data ./files/test_negative.dat neg
-
-
 
 # Prune the negative samples
 python src/prune_negative.py files/train_negative.dat ./files/train_negative_prune.dat ./src/blacklist.dat train

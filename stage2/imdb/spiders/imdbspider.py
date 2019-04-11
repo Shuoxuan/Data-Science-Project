3a7348701e573278260f21a9e84f21bdbb7e02b1
Shuoxuan Dong <30633351+Shuoxuan@users.noreply.github.com>
2019-04-11 15:31:59 -0500

    Add files via upload

--- stage2/imdb/spiders/imdbspider.py
+++ stage2/imdb/spiders/imdbspider.py
@@ -13,10 +13,8 @@ class ImdbSpider(CrawlSpider):
     )
 
     def start_requests(self):
-        # for i in range(1, 3051, 50):
-        #     url = "https://www.imdb.com/search/title?title_type=feature&release_date=2018-05-01,2019-05-01&start=" + str(i)
-        #     yield Request(url=url, callback=self.parse)
-        url = "https://www.imdb.com/search/title?title_type=feature&release_date=2018-05-01,2019-05-01"
+        #for i in range(1, 5):
+        url = "https://www.imdb.com/search/title?title_type=feature&release_date=2018-01-01,2019-05-01"
         yield Request(url=url, callback=self.parse)
 
     def parse_imdb(self, response):
@@ -25,10 +23,9 @@ class ImdbSpider(CrawlSpider):
             item['video_url'] = response.url
             item['video_title'] = "".join(response.xpath('//*[@class="title_wrapper"]/h1/text()').extract())
             item['video_year'] = "".join(response.xpath('//*[@id="titleYear"]/a/text()').extract())
-            item['video_level'] = "".join(response.xpath('//*[@class="subtext"]/a[1]/text()').extract())
-            item['video_length'] = "".join(response.xpath('//*[@class="subtext"]/a[2]/text()').extract())
-            item['video_genres'] = "".join(response.xpath('//*[@class="subtext"]/a[3]/text()').extract())
-            item['video_releasedate'] = "".join(response.xpath('//*[@class="subtext"]/a[4]/text()').extract())
+
+            item['video_level'] = "".join(response.xpath('//*[@class="subtext"]/text()').extract())
+            item['video_genres'] = "".join(response.xpath('//*[@class="subtext"]/a/text()').extract())
             yield item
         except Exception as error:
-            print(error)+
+            log(error)

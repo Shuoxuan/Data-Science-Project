483490041bd87289c290aa57e1611abf605cdce6
Shuoxuan <sdong34@wisc.edu>
2019-04-23 14:16:08 -0500

    update

--- Stage3/DataCleaning.py
+++ Stage3/DataCleaning.py
@@ -5,13 +5,23 @@ def splitGenres(dirty):
     #insert spaces
     dirty = re.sub(r"(\w)([A-Z])", r"\1 \2", dirty)
     dirty = dirty.replace("(", "")
+    dirty = dirty.replace("U SA)", "")
+
     m = re.search("\d", dirty)
 
     if m:
         pos = m.start()
-        return dirty[:pos], dirty[pos:]
+        return dirty[:pos], dirty[pos:].rstrip().replace(" ", "-")
 
     return dirty, ""
+
+def splitLevel(dirty):
+    m = re.search(",", dirty)
+    if m:
+        pos = m.start()
+        return dirty[:pos]
+    return dirty
+
 
 
 with open('./imdb.csv') as csv_file:
@@ -26,14 +36,16 @@ with open('./imdb.csv') as csv_file:
                 spamwriter.writerow(row)
                 line_count += 1
             else:
-                genres, release_date = splitGenres(row[0])
+                genres, release_date = splitGenres(row[0].strip())
                 row.append(release_date)
                 row[0] = genres
-                row[1] = row[1].strip()
+                row[1] = splitLevel(row[1].strip()).rstrip()
                 row[2] = row[2].strip()
                 row[3] = row[3].strip()
                 row[4] = row[4].strip()
                 row[5] = row[5].strip()
-                spamwriter.writerow(row)
-                line_count += 1
+
+                if "TV" not in row[1] and "TV" not in row[0]:
+                    spamwriter.writerow(row)
+                    line_count += 1
         print(f'Processed {line_count} lines.')
